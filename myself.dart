c612cc816e89203ec1d6ffb27867d5845966d62f
Ntshangase <simphiwe323i@gmail.com>
2022-05-23 09:58:53 +0000

    Upload New File

---
+++ myself.dart
@@ -0,0 +1,13 @@
+
+void main () {
+
+var name = "Simphiwe";
+var favouriteApp = "Netflix";
+var favouriteCity = "Magudu";
+
+print("my name is $name");
+print("my favourite app is $favouriteApp");
+print("my favourite city is $favouriteCity");
+
+
+} 
